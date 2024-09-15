2ec16bd3f30f407be62a9bdedd8f95df1f166006
Carlos Avila <35896114-cavila12@users.noreply.replit.com>
2024-09-15 21:36:58 +0000

    13

--- main.cs
+++ main.cs
@@ -4,7 +4,13 @@ class Program {
   static void Main ()
   {
     const double baseRate = 200.00;
-    const double ratePerHour = 150.00;
-    
+    const double hourlyRate = 150.00;
+    const double perMileRate = 2.00;
+    Console.WriteLine("Enter the estimated number of hours: ");
+    double hours = Convert.ToDouble(Console.ReadLine());
+     Console.Write("Enter the estimated number of miles: ");
+    double miles = Convert.ToDouble(Console.ReadLine());
+    double totalFee = baseRate + (hourlyRate * hours) + (perMileRate * miles);
+  Console.WriteLine($"The total moving fee is: {totalFee:C2}");
   }
 }
